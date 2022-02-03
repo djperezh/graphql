fcdf0f3fd54e98aeeaefc167cb420e8b16bd6eda
Dom Perez <djperezh@gmail.com>
2022-02-03 15:16:20 -0800

    Include commented code

--- client/index.js
+++ client/index.js
@@ -2,22 +2,34 @@ const graphqlRequest = require('./myModule.js')
 const gql = require("graphql-request").gql;
 
 // This could be set by config values
-const endpoint = 'http://127.0.0.1:8080/graphql';
-const query = gql`
-    {
-        getAliens {
-            id
-            firstName
-            lastName
-            planet
-        } 
+// const endpoint = 'http://127.0.0.1:8080/graphql';
+// const query = gql`
+//     {
+//         getAliens {
+//             id
+//             firstName
+//             lastName
+//             planet
+//         } 
+//     }
+// `
+
+const endpoint = 'https://graphql.contentful.com/content/v1/spaces/mt0pmhki5db7';
+const query = gql`{
+    showCollection {
+      items { title }
     }
+  }
 `
 
 // GraphQL API function
 graphqlRequest(endpoint, query).then(function(result) {
-    // Print to console
-    result.getAliens.forEach(alien => {
-        console.log(`[${alien.id}] ${alien.firstName} (from: ${alien.planet})`)
+    result.showCollection.items.forEach(item => {
+        console.log(item.title)
     });
+
+    // Print to console
+    // result.getAliens.forEach(alien => {
+    //     console.log(`[${alien.id}] ${alien.firstName} (from: ${alien.planet})`)
+    // });
 });
--- client/myModule.js
+++ client/myModule.js
@@ -1,8 +1,22 @@
 // from https://github.com/prisma-labs/graphql-request
 
-const gql = require("graphql-request").gql;
-const request = require("graphql-request").request;
+//const gql = require("graphql-request").gql;
+//const request = require("graphql-request").request;
+const graphQLClient = require("graphql-request");
 
 module.exports = async function (endpoint, query) {
-    return await request(endpoint, query)
+    const qry = graphQLClient.gql`{
+        showCollection {
+          items { title }
+        }
+      }
+    `;
+
+    const client = new graphQLClient.GraphQLClient(endpoint, {
+        headers: {
+            authorization: "Bearer 8c7dbd270cb98e83f9d8d57fb8a2ab7bac9d7501905fb013c69995ebf1b2a719",
+        },
+    });
+    //return await request(endpoint, query)
+    return await client.request(qry);
 };
